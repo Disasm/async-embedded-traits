708eb2d603918b3f28a011d792702f0719d80e19
Vadim Kaushan <admin@disasm.info>
2020-06-20 17:19:04 +0300

    Add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@
 #![feature(generic_associated_types)]
 
 pub mod delay;
+pub mod prelude;
 pub mod serial;
 pub mod spi;
---
+++ src/prelude.rs
@@ -0,0 +1,16 @@
+//! The prelude is a collection of all the traits in this crate
+//!
+//! The traits have been renamed to avoid collisions with other items when
+//! performing a glob import.
+
+pub use crate::delay::{
+    AsyncDelayMs as _async_embedded_traits_delay_AsyncDelayMs,
+    AsyncDelayUs as _async_embedded_traits_delay_AsyncDelayUs
+};
+
+pub use crate::serial::{
+    AsyncRead as _async_embedded_traits_serial_AsyncRead,
+    AsyncWrite as _async_embedded_traits_serial_AsyncWrite
+};
+
+pub use crate::spi::AsyncTransfer as _async_embedded_traits_spi_AsyncTransfer;
